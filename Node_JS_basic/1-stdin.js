f2bd1916080a6bb0f5ff396edfae61c0a5ef87c0
HaroldS10 <haroldsanty13@gmail.com>
2024-02-13 16:30:50 -0500

    upload second progress to file 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,19 @@
 //
 
 function StdinName() {
-    process.stdin.setEncoding('utf8');
-    process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.setEncoding('utf8');
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-    process.stdin.once('readable', () => {
-        const userInput = process.stdin.read();
-        if (userInput !== null && userInput !== '\n') {
-            process.stdout.write('Your name is: ' + userInput);
-        } 
-        process.stdin.emit('end');
-    });
+  process.stdin.once('readable', () => {
+    const userInput = process.stdin.read();
+    if (userInput !== null && userInput !== '\n') {
+      process.stdout.write('Your name is: ' + userInput);
+    }
+    process.stdin.emit('end');
+  });
 
-    process.stdin.once('end', function() {
-       process.stdout.write('This important software is now closing\n');
-    });
+  process.stdin.once('end', function () {
+    process.stdout.write('This important software is now closing\n');
+  });
 }
 StdinName();
