a4d7e09a81113d5e02089cb33404ae760b04a3ab
HaroldS10 <haroldsanty13@gmail.com>
2024-02-15 15:31:46 -0500

    upload three progress to file 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const [, ...students] = lines;
+
+    const fieldCounts = {};
+
+    students.forEach((student) => {
+      const [, , , field] = student.split(',');
+      if (fieldCounts[field]) {
+        fieldCounts[field].push(student.split(',')[0]);
+      } else {
+        fieldCounts[field] = [student.split(',')[0]];
+      }
+    });
+
+    console.log(`Number of students: ${students.length}`);
+    for (const field in fieldCounts) {
+      console.log(`Number of students in ${field}: ${fieldCounts[field].length}. List: ${fieldCounts[field].join(', ')}`
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
